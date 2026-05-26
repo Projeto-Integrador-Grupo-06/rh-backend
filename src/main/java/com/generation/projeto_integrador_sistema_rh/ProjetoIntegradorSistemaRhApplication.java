ad168fa1ad029b1cd1d7be9bf67ffc8914a8b450
Jhonatan Oliveira <oliveirajhonatanjesus18@gmail.com>
2026-05-26 11:55:15 -0300

    Inicio do projeto

---
+++ src/main/java/com/generation/projeto_integrador_sistema_rh/ProjetoIntegradorSistemaRhApplication.java
@@ -0,0 +1,13 @@
+package com.generation.projeto_integrador_sistema_rh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegradorSistemaRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegradorSistemaRhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/projeto_integrador_sistema_rh/ProjetoIntegradorSistemaRhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.projeto_integrador_sistema_rh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIntegradorSistemaRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
